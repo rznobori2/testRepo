b8fa5079c80cb490bb077806e27e4530246c76fb
s.nobori <nobori@xxx.com>
2016-01-29 18:15:25 +0900

    delete

--- test2/load.js
+++
@@ -1,6 +0,0 @@
-function load() {
-
-	  repo.loadAs("blob", "557db03de997c86a4a028e1ebd3a1ceb225be238", function(err, byteArray){
-	    console.log(byteArray);
-	  });
-}
--- test2/main.js
+++
@@ -1,6 +0,0 @@
-window.Github = require('github-api');
-window.github = new Github({
-  username: "rznobori",
-  password: "ns000000",
-  auth: "basic"
-});
